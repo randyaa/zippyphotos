e1887174b26b27c15df5e8c6d79ce4a58815ed23
randyaa <randyaa@abd55931-212f-0410-9c59-2db84736bb93>
2007-04-28 01:32:08 +0000

    initial revision

---
+++ zippyphotos/src/php/settings.php
@@ -0,0 +1,13 @@
+<?php 
+
+$galleryroot= 'test/';
+$imageroot= 'imgs/';
+$thumbsroot = 'thumbs/';
+$thumbsize = 200;
+$intermediatesize = 800;
+$imagesperrow = 3;
+$rowsperpage = 1;
+
+$_SERVER['GALLERY_ROOT'] = $galleryroot;
+
+?>

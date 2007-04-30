74a1261bf6a866a24d42acf5abd0b754401d3c15
randyaa <randyaa@abd55931-212f-0410-9c59-2db84736bb93>
2007-04-30 01:47:50 +0000

    added global image_root, thumb_root and thumb_album(where applicable) properties.  changed rowsperpage default to 3.  this provides a nice 3x3 grid.  added a flag to set whether the thumbnail directory exists within the image directory.  this allows the thumbnail directory to not be rendered.

--- trunk/src/php/settings.php
+++ trunk/src/php/settings.php
@@ -1,13 +1,22 @@
 <?php 
 
-$galleryroot= 'test/';
-$imageroot= 'imgs/';
-$thumbsroot = 'thumbs/';
+$galleryroot= '';
+$imageroot= '';
+$thumbsroot = 'newthumbs/';
+$thumbsinsideimageroot=TRUE;
 $thumbsize = 200;
 $intermediatesize = 800;
 $imagesperrow = 3;
-$rowsperpage = 1;
+$rowsperpage = 3;
 
 $_SERVER['GALLERY_ROOT'] = $galleryroot;
+$_SERVER['IMAGE_ROOT'] = $imageroot;
+$_SERVER['THUMB_ROOT'] = $thumbsroot;
+
+/* TODO fix this.  this forces two locations to set the thumbnail directory.  BAD!! */
+if ($thumbsinsideimageroot) {
+	$_SERVER['THUMB_ALBUM'] = 'newthumbs';
+}
+
 
 ?>

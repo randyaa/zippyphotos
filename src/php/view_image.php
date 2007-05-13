4cd46a3b52e9ff03dcb6a28ce26c364820c05ab6
randyaa <randyaa@abd55931-212f-0410-9c59-2db84736bb93>
2007-05-13 01:54:39 +0000

    cleanup

--- src/php/view_image.php
+++ src/php/view_image.php
@@ -3,24 +3,15 @@
 include("settings.php");
 include("utilities.php");
 
+$imagefolder=$_SERVER['IMAGE_ROOT'].$_SERVER['ALBUM_FOLDER'];
+$thumbsfolder=$_SERVER['THUMB_ROOT'].$_SERVER['ALBUM_FOLDER'];
 
-if (isset($_GET['album'])){
-	$albumfolder = $_GET['album'];
-} else {
-	$albumfolder = "";
-}
-if (isset($_GET['image'])) {
-	$image = $_GET['image'];
-}
-
-$imagefolder=$imageroot.$albumfolder;
-$thumbsfolder=$thumbsroot.$albumfolder;
-
-echo "<a href='".currentURL()."/view_album.php'>home</a>";
-if ($albumfolder != "" ) {
+echo "<a href='".currentURL()."/view_album.php'>Home</a>";
+if ($_SERVER['ALBUM_FOLDER'] != "" ) {
 	echo " &gt; ";
-	displayHeirarchy($albumfolder);
+	displayHeirarchy($_SERVER['ALBUM_FOLDER']);
 }
+
 echo "<br/><br/>";
 
 if (!isset($image)) {

1ef7542095da3c02654f11744d03a17ea509c49f
randyaa <randyaa@abd55931-212f-0410-9c59-2db84736bb93>
2007-05-16 05:17:23 +0000

    abstracted the rss settings. Also changed the description for each item from a repeated link to the message "from <a href='link to site'>site name</a>"

--- src/php/rss.php
+++ src/php/rss.php
@@ -1,9 +1,9 @@
 <? 
 include("settings.php");
-include("settings.php");
+include("utilities.php");
 
 // get your news items from somewhere, e.g. your database:
-$myFile = "RSS.txt";
+$myFile = $_SERVER['RSS_FILE'];
 $fh = fopen($myFile, 'r');
 $theData = fread($fh, filesize($myFile));
 fclose($fh);
@@ -11,16 +11,16 @@ fclose($fh);
 
 echo "<rss version='2.0'>";
 echo "<channel>";
-echo "<title>Randy and Rachel's Photos</title>";
+echo "<title>".$_SERVER['WEBSITE_TITLE']."</title>";
 echo "<link>".currentURL()."</link>";
-echo "<description>Randy and Rachel's Photos.</description>";
+echo "<description>".$_SERVER['WEBSITE_DESCRIPTION']."</description>";
 
 preg_match_all ("/<album>.*<name>(.*)<\/name>.*<link>(.*)<\/link>.*<\/album>/", $theData, $matches, PREG_SET_ORDER);
 
 $reversedmatches = array_reverse($matches);
 
 foreach ($reversedmatches as $val) {
-	writeItem($val[1],$val[2],$val[2]);	
+	writeItem($val[1],$val[2],"from <a href='".currentURL()."'>".$_SERVER['HTTP_HOST']."</a>");	
 }
 
 echo "</channel>";

d43d2bf9f6cd7b9484cb9e3917d4fadefcc94ede
randyaa <randyaa@abd55931-212f-0410-9c59-2db84736bb93>
2007-05-19 02:12:39 +0000

    dramatically increased spead and improved memory usage with new  fastimagecopyresampled function.

--- src/php/utilities.php
+++ src/php/utilities.php
@@ -321,7 +321,8 @@ function createthumb($name,$filename,$new_w,$new_h) {
 	}
 	
 	$dst_img=ImageCreateTrueColor($thumb_w,$thumb_h);
-	imagecopyresampled($dst_img,$src_img,0,0,0,0,$thumb_w,$thumb_h,$old_x,$old_y); 
+	fastimagecopyresampled($dst_img,$src_img,0,0,0,0,$thumb_w,$thumb_h,$old_x,$old_y);
+	/*imagecopyresampled($dst_img,$src_img,0,0,0,0,$thumb_w,$thumb_h,$old_x,$old_y); */
 	if (preg_match("/png/",$system[1])) {
 		imagepng($dst_img,$filename); 
 	} else {
@@ -331,3 +332,34 @@ function createthumb($name,$filename,$new_w,$new_h) {
 	imagedestroy($src_img); 
 }
 
+function fastimagecopyresampled (&$dst_image, $src_image, $dst_x, $dst_y, $src_x, $src_y, $dst_w, $dst_h, $src_w, $src_h, $quality = 2) {
+  // Plug-and-Play fastimagecopyresampled function replaces much slower imagecopyresampled.
+  // Just include this function and change all "imagecopyresampled" references to "fastimagecopyresampled".
+  // Typically from 30 to 60 times faster when reducing high resolution images down to thumbnail size using the default quality setting.
+  // Author: Tim Eckel - Date: 12/17/04 - Project: FreeRingers.net - Freely distributable.
+  //
+  // Optional "quality" parameter (defaults is 3).  Fractional values are allowed, for example 1.5.
+  // 1 = Up to 600 times faster.  Poor results, just uses imagecopyresized but removes black edges.
+  // 2 = Up to 95 times faster.  Images may appear too sharp, some people may prefer it.
+  // 3 = Up to 60 times faster.  Will give high quality smooth results very close to imagecopyresampled.
+  // 4 = Up to 25 times faster.  Almost identical to imagecopyresampled for most images.
+  // 5 = No speedup.  Just uses imagecopyresampled, highest quality but no advantage over imagecopyresampled.
+
+  if (empty($src_image) || empty($dst_image)) { return false; }
+  if ($quality <= 1) {
+    $temp = imagecreatetruecolor ($dst_w + 1, $dst_h + 1);
+    imagecopyresized ($temp, $src_image, $dst_x, $dst_y, $src_x, $src_y, $dst_w + 1, $dst_h + 1, $src_w, $src_h);
+    imagecopyresized ($dst_image, $temp, 0, 0, 0, 0, $dst_w, $dst_h, $dst_w, $dst_h);
+    imagedestroy ($temp);
+  } elseif ($quality < 5 && (($dst_w * $quality) < $src_w || ($dst_h * $quality) < $src_h)) {
+    $tmp_w = $dst_w * $quality;
+    $tmp_h = $dst_h * $quality;
+    $temp = imagecreatetruecolor ($tmp_w + 1, $tmp_h + 1);
+    imagecopyresized ($temp, $src_image, $dst_x * $quality, $dst_y * $quality, $src_x, $src_y, $tmp_w + 1, $tmp_h + 1, $src_w, $src_h);
+    imagecopyresampled ($dst_image, $temp, 0, 0, 0, 0, $dst_w, $dst_h, $tmp_w, $tmp_h);
+    imagedestroy ($temp);
+  } else {
+    imagecopyresampled ($dst_image, $src_image, $dst_x, $dst_y, $src_x, $src_y, $dst_w, $dst_h, $src_w, $src_h);
+  }
+  return true;
+}
